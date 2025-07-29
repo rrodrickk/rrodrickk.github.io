d1573657c943ab2184c697deb94ff082c8e981b5
rrodrickk <rodrigomrsilva06@gmail.com>
2025-07-29 18:28:23 +0000

    fechar o tasco por hoje

---
+++ style/fade.js
@@ -0,0 +1,21 @@
+// fade.js
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("fade-in");
+
+  document.querySelectorAll("a[href]").forEach(link => {
+    // only apply to internal links
+    if (link.hostname === location.hostname && !link.target) {
+      link.addEventListener("click", e => {
+        e.preventDefault();
+        const href = link.href;
+
+        document.body.classList.remove("fade-in");
+        document.body.classList.add("fade-out");
+
+        setTimeout(() => {
+          window.location.href = href;
+        }, 400); // matches the transition duration
+      });
+    }
+  });
+});
